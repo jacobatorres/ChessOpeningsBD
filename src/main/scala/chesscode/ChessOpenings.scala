ce56b9deebb03ac162bbf7c30ce396378e9d8f88
Jacob Torres <jacobtorres@jacobs-mbp.home>
2021-02-10 21:27:18 -0800

    Test mapPartitions

--- src/main/scala/chesscode/ChessOpenings.scala
+++ src/main/scala/chesscode/ChessOpenings.scala
@@ -2,6 +2,8 @@ package chesscode
 
 import org.apache.spark.sql._
 import org.apache.log4j._
+import org.apache.spark._
+
 import scala.io.Source
 
 object ChessOpenings {
@@ -16,11 +18,20 @@ object ChessOpenings {
     // i think sc.textFile automatically does the partitioning, and spark handles the number of partitions
     // if true, can I make a function per partition so that it's faster?
 
-    
-    for (line <- Source.fromFile("data/TestData901lines.txt").getLines){
-      println(line)
-    }
+    // as per these references:
+    // https://stackoverflow.com/questions/40892080/how-to-use-mappartitions-in-spark-scala
+    // https://spark.apache.org/docs/2.1.0/programming-guide.html (ctrl + f textfile)
+    // i should be able to do sc.textfile().mappartitions
+    // the idea is that you split the big text file into partitions,
+    // distribute these partitions into the workers, then do the function per partition
+    // much faster than just having one worker grind everything
 
+    val sc = new SparkContext ("local[*]", "Chess")
+    val ChessRDD =  sc.textFile("data/TestData901lines.txt")
+
+    val testprint = ChessRDD.mapPartitions(idx => Array(idx.size).iterator).collect
+
+    testprint.foreach(println)
 
 //
 //    val spark = SparkSession
